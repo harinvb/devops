1c18a959fd6f97061e7d09f94054cdbbea6d86f0
HariBabu Nalluri <harinvb@gmail.com>
2021-09-09 08:13:40 +0530

    Changes

--- src/main/java/org/mindtree/devops/controller/MessageController.java
+++ src/main/java/org/mindtree/devops/controller/MessageController.java
@@ -1,12 +1,12 @@
 package org.mindtree.devops.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String version() {
 		return "0.0.1";
 	}
--- src/test/java/org/mindtree/devops/DevopsApplicationTests.java
+++ src/test/java/org/mindtree/devops/DevopsApplicationTests.java
@@ -1,5 +1,7 @@
 package org.mindtree.devops;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,10 @@ class DevopsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int var =21;
+		var-=20;
+		var-=1;
+		assertEquals(0, var);
 	}
 
 }
